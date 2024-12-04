6ea939c467150e0df9df7659f681548e00e8eb23
NidiMeliodas <nidigashan@gmail.com>
2024-12-04 16:17:31 +0100

    push all

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+# Bash script that initializes, builds, and runs a new C# project from a folderdotnet new console -o 2-new_project
+dotnet new console -o 2-new_project
+cd 2-new_project
+dotnet build
+dotnet run
