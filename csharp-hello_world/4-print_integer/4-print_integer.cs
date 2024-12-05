ac3fb07ae66c5223189cb44caca57b26832e2379
NidiMeliodas <nidigashan@gmail.com>
2024-12-05 16:10:45 +0100

    project 1 task 4

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,10 @@
+using System;
+// Print the integer stored in the variable number, followed by Mission Street followed by a new line
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine($"\"{number} Mission Street");
+	}
+}
