a9ca784d7491b321f447eda04466149221bae652
NidiMeliodas <nidigashan@gmail.com>
2024-12-05 16:10:32 +0100

    adding arg

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,7 +1,7 @@
 using System;
 class Program
 {
-	static void Main()
+	static void Main(string[] args)
 	{
 		// C# script that prints exactly "Programming is like building a multilingual puzzle followed by a new line.
 
