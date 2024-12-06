091be8fc55e216ff87e9ed5c8911e019d691efb9
NidiMeliodas <nidigashan@gmail.com>
2024-12-06 11:36:55 +0100

    fix ?

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -4,7 +4,6 @@ class Program
 	static void Main(string[] args)
 	{
 		// C# script that prints exactly "Programming is like building a multilingual puzzle followed by a new line.
-
 		Console.WriteLine("\"Programming is like building a multilingual puzzle");
 	}
 }
