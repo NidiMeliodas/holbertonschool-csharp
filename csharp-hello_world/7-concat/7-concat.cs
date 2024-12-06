76a32971b6ab4900213505c34542195a7b130e6b
NidiMeliodas <nidigashan@gmail.com>
2024-12-06 15:19:48 +0100

    fixing concat method

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		string str1 = "Holberton";
 		string str2 = "School";
-		string str3= String.Concat(str1, str2);
-		Console.WriteLine("Welcome to {0}!", str3);
+		str1 += " " + str2;
+		Console.WriteLine("Welcome to {0}!", str1);
 	}
 }
