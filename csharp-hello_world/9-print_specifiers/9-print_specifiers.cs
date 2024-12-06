3167a95f76f87d28cfe83d2cd23549f2eca6cf53
NidiMeliodas <nidigashan@gmail.com>
2024-12-06 16:17:59 +0100

    fix task 9

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -4,9 +4,9 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		double percent = .7553;
-		double currency = 98765.4321;
+		double percent = .7553,currency = 98765.4321;   // This should be 75.53%
 		CultureInfo culture = new CultureInfo("en-US");
-		Console.WriteLine(string.Format(culture, "Percent: {0:P2} \nCurrency: {1:C2} ", percent, currency));
+		Console.WriteLine(string.Format(culture, "Percent: {0:P2}", percent));// Printing the percent with two decimal places in percentage format
+		Console.WriteLine(string.Format(culture, "Currency: {0:C2}", currency));// Printing the currency with two decimal places in currency format
 	}
 }
