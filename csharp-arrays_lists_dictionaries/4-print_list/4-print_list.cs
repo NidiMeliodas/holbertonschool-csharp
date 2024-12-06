6edaa369464ff6df3cf0494fd2caf301e4f12082
NidiMeliodas <nidigashan@gmail.com>
2024-12-06 23:52:32 +0100

    task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+		}
+
+		List<int> newList = new List<int>();
+
+		for (int i = 0; i < size; i++)
+		{
+			newList.Add(i);
+		}
+		
+		// Print the list without trailing space
+		for (int i = 0; i < newList.Count; i++)
+		{
+			Console.Write(newList[i]);
+			if (i < newList.Count - 1)
+			{
+				Console.Write(" ");
+			}
+		}
+		Console.WriteLine();
+
+		return newList;
+	}
+}
