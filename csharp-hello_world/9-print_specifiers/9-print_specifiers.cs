ed8aefe3aa2969ec6eb468babd1c156569249395
NidiMeliodas <nidigashan@gmail.com>
2024-12-06 16:07:55 +0100

    fix 9

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,17 +1,12 @@
 using System;
 using System.Globalization;
-
 class Program
 {
 	static void Main(string[] args)
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-
-		// Définit la culture explicitement
 		CultureInfo culture = new CultureInfo("en-US");
-
-		// Formatage avec culture
 		Console.WriteLine(string.Format(culture, "Percent: {0:P2} \nCurrency: {1:C2}\n", percent, currency));
 	}
 }
