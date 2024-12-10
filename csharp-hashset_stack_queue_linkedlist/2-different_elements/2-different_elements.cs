8067d08a1ffcca67f8959ddf63659d813ff6f5bc
NidiMeliodas <nidigashan@gmail.com>
2024-12-10 15:50:16 +0100

    task 2

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class List
 {
-    static void Main(string[] args)
+    // Method to find elements present in one list or the other but not both
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        Console.WriteLine("Hello from 2-different_elements!");
+        // HashSet to track elements that are different
+        HashSet<int> differenceSet = new HashSet<int>();
+
+        // HashSet to track elements already seen in both lists
+        HashSet<int> seenSet = new HashSet<int>();
+
+        // Add elements from the first list
+        foreach (int num in list1)
+        {
+            if (!seenSet.Add(num))
+            {
+                // If it's already in seenSet, remove from differenceSet
+                differenceSet.Remove(num);
+            }
+            else
+            {
+                differenceSet.Add(num);
+            }
+        }
+
+        // Add elements from the second list
+        foreach (int num in list2)
+        {
+            if (!seenSet.Add(num))
+            {
+                // If it's already in seenSet, remove from differenceSet
+                differenceSet.Remove(num);
+            }
+            else
+            {
+                differenceSet.Add(num);
+            }
+        }
+
+        // Convert the differenceSet to a List and sort it
+        List<int> result = new List<int>(differenceSet);
+        result.Sort();
+
+        return result;
     }
 }
