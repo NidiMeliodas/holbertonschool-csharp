02abdab68cf9768f3018bb769a1b909989da0a74
NidiMeliodas <nidigashan@gmail.com>
2024-12-10 11:57:22 +0100

    task 1

--- csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class List
 {
-    static void Main(string[] args)
+    // Method to find common elements in two lists and return them in sorted order
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
     {
-        Console.WriteLine("Hello from 1-common_elements!");
+        // Create a HashSet to store common elements
+        HashSet<int> commonSet = new HashSet<int>();
+
+        // Iterate through the first list and add the elements to the HashSet
+        foreach (int num in list1)
+        {
+            // If the second list contains the number, add it to the commonSet
+            if (list2.Contains(num))
+            {
+                commonSet.Add(num);
+            }
+        }
+
+        // Convert the HashSet to a List for sorting
+        List<int> commonList = new List<int>(commonSet);
+
+        // Sort the list
+        commonList.Sort();
+
+        // Return the sorted list of common elements
+        return commonList;
     }
 }
