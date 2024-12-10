aaddcdd3360cff5cb700d355c031f8ad9d5f8292
NidiMeliodas <nidigashan@gmail.com>
2024-12-10 15:58:10 +0100

    task 3

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+public class MyStack
 {
-    static void Main(string[] args)
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine("Hello from 3-stack_push_pop!");
+        // Print the number of items in the stack
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        // Check if the stack is empty
+        if (aStack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            // Print the item at the top of the stack without removing it
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+
+        // Check if the stack contains the search item
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine($"Stack contains {search}: {containsSearch}");
+
+        // If the stack contains the search item, remove all items up to and including it
+        if (containsSearch)
+        {
+            Stack<string> tempStack = new Stack<string>();
+
+            // Transfer items to a temporary stack until the search item is found
+            while (aStack.Count > 0)
+            {
+                string currentItem = aStack.Pop();
+                if (currentItem == search)
+                {
+                    break; // Stop once the search item is found
+                }
+                tempStack.Push(currentItem);
+            }
+
+            // Return items from the temporary stack to the original stack
+            while (tempStack.Count > 0)
+            {
+                aStack.Push(tempStack.Pop());
+            }
+        }
+
+        // Add the new item to the stack
+        aStack.Push(newItem);
+
+        // Return the modified stack
+        return aStack;
     }
+
 }
