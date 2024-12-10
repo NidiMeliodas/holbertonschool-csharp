a9fcc144f6caff8d1b390c7a41710ee01fd8b6b5
NidiMeliodas <nidigashan@gmail.com>
2024-12-10 16:41:06 +0100

    queue de

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,55 +1,28 @@
 using System;
 using System.Collections.Generic;
 
-public class MyQueue
+class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        // Print the number of items in the queue
         Console.WriteLine($"Number of items: {aQueue.Count}");
 
-        // Check if the queue is empty
         if (aQueue.Count == 0)
         {
             Console.WriteLine("Queue is empty");
         }
         else
         {
-            // Print the first item in the queue without removing it
             Console.WriteLine($"First item: {aQueue.Peek()}");
         }
-
-        // Check if the queue contains the search item
-        bool containsSearch = aQueue.Contains(search);
-        Console.WriteLine($"Queue contains {search}: {containsSearch}");
-
-        // If the queue contains the search item, remove items up to and including it
-        if (containsSearch)
-        {
-            Queue<string> tempQueue = new Queue<string>();
-
-            // Transfer items to a temporary queue until the search item is found
-            while (aQueue.Count > 0)
-            {
-                string currentItem = aQueue.Dequeue();
-                if (currentItem == search)
-                {
-                    break; // Stop once the search item is found
-                }
-                tempQueue.Enqueue(currentItem);
-            }
-
-            // Return remaining items from the temporary queue back to the original queue
-            while (tempQueue.Count > 0)
-            {
-                aQueue.Enqueue(tempQueue.Dequeue());
-            }
-        }
-
-        // Add the new item to the queue
         aQueue.Enqueue(newItem);
 
-        // Return the modified queue
+        Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
+
+        if (aQueue.Contains(search))
+        {
+            while (aQueue.Dequeue() != search) ;
+        }
         return aQueue;
     }
 }
