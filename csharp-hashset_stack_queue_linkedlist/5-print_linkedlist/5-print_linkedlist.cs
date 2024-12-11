4d2e8b385ff550ccadebd9cecf80d2fd202912e9
NidiMeliodas <nidigashan@gmail.com>
2024-12-11 13:13:49 +0100

    task 5

--- csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class LList
 {
-    static void Main(string[] args)
+    public static LinkedList<int> CreatePrint(int size)
     {
-        Console.WriteLine("Hello from 5-print_linkedlist!");
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        // Handle negative size by returning an empty LinkedList
+        if (size < 0)
+        {
+            return linkedList;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            linkedList.AddLast(i);
+            Console.WriteLine(i); // Print the integer as it's added
+        }
+
+        return linkedList;
     }
 }
