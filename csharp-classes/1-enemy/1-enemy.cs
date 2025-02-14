e239b718f4c87232be72f987c0d2d681c1b09eec
NidiMeliodas <nidigashan@gmail.com>
2025-02-14 11:19:41 +0100

    task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,21 @@
+namespace Enemies
+{
+	/// <summary>
+	/// Represents a Zombie enemy.
+	/// </summary>
+	public class Zombie
+	{
+		/// <summary>
+		/// Health of the Zombie.
+		/// </summary>
+		public int health;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Zombie"/> class with health set to 0.
+		/// </summary>
+		public Zombie()
+		{
+			health = 0;
+		}
+	}
+}
