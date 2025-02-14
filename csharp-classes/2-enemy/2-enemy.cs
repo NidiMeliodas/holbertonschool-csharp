5cea22c01600bc667989b5b430c976cd29c1ff89
NidiMeliodas <nidigashan@gmail.com>
2025-02-14 11:31:37 +0100

    fix

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -1,28 +1,18 @@
+using System;
+
 namespace Enemies
 {
-	/// <summary>
-	/// Represents a Zombie enemy.
-	/// </summary>
 	public class Zombie
 	{
-		/// <summary>
-		/// Health of the Zombie.
-		/// </summary>
 		public int health;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Zombie"/> class with health set to 0.
-		/// </summary>
+		// Default constructor
 		public Zombie()
 		{
 			health = 0;
 		}
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Zombie"/> class with a specified health value.
-		/// </summary>
-		/// <param name="value">Initial health value.</param>
-		/// <exception cref="ArgumentException">Thrown when value is less than 0.</exception>
+		// Parameterized constructor
 		public Zombie(int value)
 		{
 			if (value < 0)
