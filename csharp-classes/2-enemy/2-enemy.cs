2450e9dfb458f40aba4d8362293664ed0348ce3c
NidiMeliodas <nidigashan@gmail.com>
2025-02-14 11:19:10 +0100

    task 2

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,35 @@
+namespace Enemies
+{
+	/// <summary>
+	/// Represents a Zombie enemy.
+	/// </summary>
+	public class Zombie
+	{
+		/// <summary>
+		/// Health of the Zombie.
+		/// </summary>
+		public int health;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Zombie"/> class with health set to 0.
+		/// </summary>
+		public Zombie()
+		{
+			health = 0;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Zombie"/> class with a specified health value.
+		/// </summary>
+		/// <param name="value">Initial health value.</param>
+		/// <exception cref="ArgumentException">Thrown when value is less than 0.</exception>
+		public Zombie(int value)
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			}
+			health = value;
+		}
+	}
+}
