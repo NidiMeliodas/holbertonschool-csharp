424d6357a8c80971480291e07b08c835411fd81f
NidiMeliodas <nidigashan@gmail.com>
2025-03-31 10:08:37 +0200

    fix

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Bash script that initializes, builds, and runs a new C# project from a folder
-dotnet new console -o 2-new_project  && dotnet build 2-new_project && dotnet run --project ./2-new_project
+dotnet new console -o 2-new_project  && dotnet build 2-new_project && dotnet run --project 2-new_project/2-new_project.csproj
