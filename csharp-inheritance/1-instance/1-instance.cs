7cf88d9b6afdfb5c51f136fd7d1212c175069860
NidiMeliodas <nidigashan@gmail.com>
2025-05-09 10:21:58 +0200

    task 1

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Provides utility methods for instance checking.
+/// </summary>
+public class Obj
+{
+	/// <summary>
+	/// Returns true if obj is an instance of Array or a class that inherits from Array.
+	/// </summary>
+	/// <param name="obj">The object to check.</param>
+	/// <returns>true if obj is an instance of Array or derived class; otherwise, false.</returns>
+	public static bool IsInstanceOfArray(object obj)
+	{
+		return obj is Array;
+	}
+}
