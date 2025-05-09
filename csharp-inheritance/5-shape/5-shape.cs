6b84d159eabc0d0d71c30100d675e9a2f7fd71cb
NidiMeliodas <nidigashan@gmail.com>
2025-05-09 10:33:14 +0200

    task 5

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,14 @@
+/// <summary>
+/// Represents a geometric shape.
+/// </summary>
+public class Shape
+{
+    /// <summary>
+    /// Calculates the area of the shape.
+    /// </summary>
+    /// <returns>The area as an integer.</returns>
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+}
