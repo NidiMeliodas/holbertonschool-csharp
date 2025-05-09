349f89844410808ab258604fee3b86e04afdb8d0
NidiMeliodas <nidigashan@gmail.com>
2025-05-09 08:58:28 +0200

    test

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,15 @@
 using System;
 
-class Obj
+/// <summary>
+/// Contains method to check if an object is of type int.
+/// </summary>
+public class Obj
 {
+    /// <summary>
+    /// Determines whether the specified object is of type int.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>true if obj is an int; otherwise, false.</returns>
     public static bool IsOfTypeInt(object obj)
     {
         return obj is int;
