3c38c5948fcbb753dfc02a603ddc43e55e62ae8d
NidiMeliodas <nidigashan@gmail.com>
2025-05-09 10:28:53 +0200

    task 4

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,15 @@
+/// <summary>
+/// Base class Animal.
+/// </summary>
+public class Animal
+{
+    // Empty base class
+}
+
+/// <summary>
+/// Derived class Dog, inherits from Animal.
+/// </summary>
+public class Dog : Animal
+{
+    // Empty derived class
+}
