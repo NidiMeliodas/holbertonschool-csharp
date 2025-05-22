dde5bdf27c65dba48f01b6d10acc02c96b141b68
NidiMeliodas <nidigashan@gmail.com>
2025-05-22 14:49:28 +0200

    20 test fix

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,46 +1,41 @@
 using System;
 
-class MatrixMath
+namespace MatrixMath
 {
-    /// <summary>
-    /// Rotates a 2D square matrix by a given angle in radians.
-    /// </summary>
-    /// <param name="matrix">The 2D square matrix to rotate.</param>
-    /// <param name="angle">The rotation angle in radians.</param>
-    /// <returns>The rotated matrix, or a matrix containing -1 if the input is invalid.</returns>
-    public static double[,] Rotate2D(double[,] matrix, double angle)
+    public class MatrixMath
     {
-        // Validate input: Check if the matrix is null or not square
-        if (matrix == null || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        /// <summary>
+        /// Rotates a 2D square matrix by a given angle in radians.
+        /// </summary>
+        /// <param name="matrix">2D square matrix (must be 2x2)</param>
+        /// <param name="angle">Angle in radians</param>
+        /// <returns>Rotated matrix, or matrix with -1 if invalid size</returns>
+        public static double[,] Rotate2D(double[,] matrix, double angle)
         {
-            return new double[,] { { -1 } };
-        }
+            if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+                return new double[,] { { -1 } };
 
-        // Rotation matrix components
-        double cosTheta = Math.Cos(angle);
-        double sinTheta = Math.Sin(angle);
+            double cos = Math.Cos(angle);
+            double sin = Math.Sin(angle);
 
-        // Create the rotation matrix
-        double[,] rotationMatrix = new double[,]
-        {
-            { cosTheta, -sinTheta },
-            { sinTheta, cosTheta }
-        };
+            // Rotation matrix
+            double[,] rot = new double[,] {
+                { cos, -sin },
+                { sin,  cos }
+            };
 
-        // Initialize the result matrix
-        double[,] rotatedMatrix = new double[2, 2];
+            // Result of multiplying rotation matrix with the input matrix
+            double[,] result = new double[2, 2];
 
-        // Apply the rotation transformation
-        for (int i = 0; i < 2; i++) // Iterate over rows
-        {
-            for (int j = 0; j < 2; j++) // Iterate over columns
+            for (int i = 0; i < 2; i++)
             {
-                rotatedMatrix[i, j] = Math.Round(
-                    matrix[i, 0] * rotationMatrix[0, j] + matrix[i, 1] * rotationMatrix[1, j], 2
-                );
+                for (int j = 0; j < 2; j++)
+                {
+                    result[i, j] = rot[i, 0] * matrix[0, j] + rot[i, 1] * matrix[1, j];
+                }
             }
-        }
 
-        return rotatedMatrix;
+            return result;
+        }
     }
 }
