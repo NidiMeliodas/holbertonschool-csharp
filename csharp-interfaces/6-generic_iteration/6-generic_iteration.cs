096ae1841827da4a7d9616a0149374ab2f9c53c7
NidiMeliodas <nidigashan@gmail.com>
2025-07-18 13:39:13 +0200

    test

--- csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
+++ csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -2,152 +2,108 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-/// <summary>Base class for all objects</summary>
+/// <summary>Base abstract class</summary>
 public abstract class Base
 {
-	/// <summary>Name of the object</summary>
 	public string name { get; set; }
 
-	/// <summary>Returns a string representation of the object</summary>
-	public override string ToString()
-	{
-		return ($"{this.name} is a {this.GetType().Name}");
-	}
+	public override string ToString() => $"{name} is a {this.GetType().Name}";
 }
 
-/// <summary>Interface for interactive objects</summary>
+/// <summary>Interface for interactive behavior</summary>
 public interface IInteractive
 {
-	/// <summary>Interact with the object</summary>
 	void Interact();
 }
 
-/// <summary>Interface for breakable objects</summary>
+/// <summary>Interface for breakable behavior</summary>
 public interface IBreakable
 {
-	/// <summary>Durability of the object</summary>
 	int durability { get; set; }
-
-	/// <summary>Break the object</summary>
 	void Break();
 }
 
-/// <summary>Interface for collectable objects</summary>
+/// <summary>Interface for collectable behavior</summary>
 public interface ICollectable
 {
-	/// <summary>Check if the object has been collected</summary>
 	bool isCollected { get; set; }
-
-	/// <summary>Collect the object</summary>
 	void Collect();
 }
 
-/// <summary>Represents a Door object</summary>
+/// <summary>Door class that is interactive</summary>
 public class Door : Base, IInteractive
 {
-	/// <summary>Constructor for Door</summary>
-	public Door(string name = "Door")
-	{
-		this.name = name;
-	}
+	public Door(string name = "Door") => this.name = name;
 
-	/// <summary>Interact with the Door</summary>
-	public void Interact()
-	{
-		Console.WriteLine("You try to open the {0}. It's locked.", this.name);
-	}
+	public void Interact() => Console.WriteLine($"You try to open the {name}. It's locked.");
 }
 
-/// <summary>Represents a Decoration object</summary>
+/// <summary>Decoration class implementing multiple interfaces</summary>
 public class Decoration : Base, IInteractive, IBreakable
 {
-	/// <summary>Durability of the decoration</summary>
 	public int durability { get; set; }
+	public bool isQuestItem;
 
-	/// <summary>Is the decoration a quest item</summary>
-	public bool isQuestItem { get; set; }
-
-	/// <summary>Constructor for Decoration</summary>
 	public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
 	{
-		if (durability <= 0)
-			throw new ArgumentException("Durability must be greater than 0");
 		this.name = name;
 		this.durability = durability;
 		this.isQuestItem = isQuestItem;
 	}
 
-	/// <summary>Interact with the decoration</summary>
 	public void Interact()
 	{
-		if (this.durability <= 0)
-			Console.WriteLine("The {0} has been broken.", this.name);
-		else if (this.isQuestItem)
-			Console.WriteLine("You look at the {0}. There's a key inside.", this.name);
+		if (durability <= 0)
+			Console.WriteLine($"The {name} has been broken.");
+		else if (isQuestItem)
+			Console.WriteLine($"You look at the {name}. There's a key inside.");
 		else
-			Console.WriteLine("You look at the {0}. Not much to see here.", this.name);
+			Console.WriteLine($"You look at the {name}. Not much to see here.");
 	}
 
-	/// <summary>Break the decoration</summary>
 	public void Break()
 	{
-		this.durability--;
-		if (this.durability > 0)
-			Console.WriteLine("You hit the {0}. It cracks.", this.name);
-		else if (this.durability == 0)
-			Console.WriteLine("You smash the {0}. What a mess.", this.name);
+		durability--;
+		if (durability > 0)
+			Console.WriteLine($"You hit the {name}. It cracks.");
+		else if (durability == 0)
+			Console.WriteLine($"You smash the {name}. What a mess.");
+		else
+			Console.WriteLine($"The {name} is already broken.");
 	}
 }
 
-/// <summary>Represents a Key object</summary>
+/// <summary>Key class that is collectable</summary>
 public class Key : Base, ICollectable
 {
-	/// <summary>Check if the key has been collected</summary>
 	public bool isCollected { get; set; }
 
-	/// <summary>Constructor for Key</summary>
 	public Key(string name = "Key", bool isCollected = false)
 	{
 		this.name = name;
 		this.isCollected = isCollected;
 	}
 
-	/// <summary>Collect the key</summary>
 	public void Collect()
 	{
-		if (!this.isCollected)
+		if (!isCollected)
 		{
-			this.isCollected = true;
-			Console.WriteLine("You pick up the {0}.", this.name);
+			isCollected = true;
+			Console.WriteLine($"You pick up the {name}.");
 		}
 		else
-		{
-			Console.WriteLine("You have already picked up the {0}.", this.name);
-		}
+			Console.WriteLine($"You have already picked up the {name}.");
 	}
 }
 
-/// <summary>Generic collection class that implements IEnumerable</summary>
+/// <summary>Generic iterable collection class</summary>
 public class Objs<T> : IEnumerable<T>
 {
-	/// <summary>Internal list of objects</summary>
-	private List<T> _objs = new List<T>();
+	private List<T> items = new List<T>();
 
-	/// <summary>Adds an item to the collection</summary>
-	public void Add(T item)
-	{
-		_objs.Add(item);
-	}
+	public void Add(T item) => items.Add(item);
 
-	/// <summary>Returns an enumerator for the collection</summary>
-	public IEnumerator<T> GetEnumerator()
-	{
-		return _objs.GetEnumerator();
-	}
+	public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
 
-	/// <summary>Returns an enumerator for the collection (non-generic)</summary>
-	IEnumerator IEnumerable.GetEnumerator()
-	{
-		return this.GetEnumerator();
-	}
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
