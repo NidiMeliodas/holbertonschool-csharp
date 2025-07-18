2f67341b0f01cc1ff911be3a5375117915156254
NidiMeliodas <nidigashan@gmail.com>
2025-07-18 10:15:16 +0200

    task 0

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,20 @@
+using System;
+
+/// <summary>
+/// Abstract class Base
+/// </summary>
+public abstract class Base
+{
+	/// <summary>
+	/// Name of the object
+	/// </summary>
+	public string name { get; set; }
+
+	/// <summary>
+	/// Returns string representation of the object
+	/// </summary>
+	public override string ToString()
+	{
+		return $"{name} is a {this.GetType().Name}";
+	}
+}
